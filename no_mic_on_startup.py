ccfe72b1a142d9a9e42c5298ed5a90e9bcde51e7
DonnerWolfBach <seby.stack@s-sch.net>
2026-03-17 06:03:44 +0100

    first failed attempt at making logs open up on startup

--- no_mic_on_startup.py
+++ no_mic_on_startup.py
@@ -8,5 +8,29 @@ from talon import Context, Module, actions, settings, app
 
 
 
-def on_launch(): actions.sound.set_microphone("None")
+def on_launch(): 
+
+    actions.sound.set_microphone("None")
+    print("Set Mic to nothing")
+    import platform
+    print("trying to open the logs in a seperate window")
+    if platform.system() == "Windows":
+
+        try:
+            log_file_path = r"~/AppData\Roaming\talon\talon.log"
+
+            command = f'Get-Content -Path "{log_file_path}" -Tail 50 -Wait'
+
+            import subprocess
+            subprocess.run(
+                ["powershell", "-Command", command], 
+                check=True,
+                creationflags=subprocess.DETACHED_PROCESS,
+                close_fds=True  
+                )
+        except Exception as e:
+            print(f"An error occurred when trying to open the logs: {e}")
+
+
+
 app.register("launch", on_launch)
